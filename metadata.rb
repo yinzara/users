9dcd260e4307110a5e7083b2f36a8d9ca820bb56
yinzara <yinzara@gmail.com>
2015-10-08 16:23:32 -0700

    Adding netbeans and updating version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'cookbooks@chef.io'
 license          'Apache 2.0'
 description      'Creates users from a databag search'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.8.3'
+version          '1.8.4'
 
 recipe           'users::default', 'Empty recipe for including LWRPs'
 recipe           'users::sysadmins', 'Create and manage sysadmin group'
@@ -13,5 +13,5 @@ recipe           'users::sysadmins', 'Create and manage sysadmin group'
   supports os
 end
 
-source_url 'https://github.com/chef-cookbooks/users' if respond_to?(:source_url)
+source_url 'https://github.com/yinzara/users' if respond_to?(:source_url)
 issues_url 'https://github.com/chef-cookbooks/users/issues' if respond_to?(:issues_url)
